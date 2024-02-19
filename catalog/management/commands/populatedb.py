f2030930957ddaf1d023e6eb64680f2676473c73
Kutalov Konstantin <kutalov.k.v@mail.ru>
2024-02-20 01:38:36 +0700

    .

--- catalog/management/commands/populatedb.py
+++ catalog/management/commands/populatedb.py
@@ -6,25 +6,27 @@ django.setup()
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from catalog.models import Product
+from catalog.models import Product, Category
 from django.core.management import call_command
 
 
 class Command(BaseCommand):
-    help = 'Populate database with new data after clearing old data'
+    help = 'Заполнение базы данных новыми данными после очистки старых данных'
 
     def handle(self, *args, **options):
-        self.stdout.write('Clearing old data...')
+        self.stdout.write('Очистка старых данных...')
         self.clear_old_data()
-        self.stdout.write('Old data cleared.')
+        self.stdout.write('Старые данные очищены.')
 
-        self.stdout.write('Populating database with new data...')
+        self.stdout.write('Заполнение базы данных новыми данными...')
         self.populate_database()
-        self.stdout.write('Database populated.')
+        self.stdout.write('База данных заполнена.')
 
     def clear_old_data(self):
         with transaction.atomic():
             Product.objects.all().delete()
+            Category.objects.all().delete()
 
     def populate_database(self):
+        call_command('loaddata', 'categories_fixture.json')
         call_command('loaddata', 'products_fixture.json')
