32534a115a3599e2171ca226fe5ba45d8fd5b428
Kutalov Konstantin <kutalov.k.v@mail.ru>
2024-03-01 08:54:39 +0700

    .

--- catalog/templatetags/custom_filters.py
+++ catalog/templatetags/custom_filters.py
@@ -2,11 +2,9 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def add_media_prefix(value):
     if value:
-        if value.startswith('/media/'):
-            return value  # Если да, просто возвращаем значение без изменений
-        else:
-            return f'/media/{value}'
+        return f'/media/{value}'
     return '#'  # Если значение пустое, возвращаем #
